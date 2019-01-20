12f599b3bf25737bf6e11c57509ebb895e019210
Luis David Santiago Santana <luisdavidsantiagosantana@gmail.com>
2019-01-19 22:48:05 -0400

    Agregamos la conexion a mysql

--- src/app.js
+++ src/app.js
@@ -2,9 +2,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const api = require('./routes/')
+const mysql = require('mysql')
+const myConneciont = require('express-myconnection')
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 app.use('/api', api)
+app.use(myConneciont(mysql, {
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    port: 3306,
+    database: 'taekwondo'
+}, 'single'))
 
 module.exports = app
