95e8c6a7dc1b1b1f859c05e1034fea43ee57088f
Luis David Santiago Santana <luisdavidsantiagosantana@gmail.com>
2019-01-25 12:21:06 -0400

    Eliminamos handlebars y agregamos Vue como framework para el FrontEnd

--- src/app.js
+++ src/app.js
@@ -2,21 +2,10 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const app = express()
-const api = require('./routes/')
-const hbs = require('express-handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
-app.engine('.hbs', hbs({
-    defaultLayout: 'default',
-    extname: '.hbs',
-    layoutsDir: path.join(__dirname, 'views/layouts')
-}))
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, 'views'))
-app.get('/', (req, res) => {
-    res.render('landing')
-})
-app.use('/api', api)
+app.use(express.static(__dirname + '/public'))
+app.use('/api', require('./routes/'))
 
 module.exports = app
