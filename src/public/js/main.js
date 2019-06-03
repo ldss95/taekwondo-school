d4fb71bcbef03c6f411d6399a658ad77c9cd3b86
Luis David Santiago Santana <luisdavidsantiagosantana@gmail.com>
2019-06-02 20:26:56 -0400

    Terminamos el swipe del menu de navegacion

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,31 +1,32 @@
-document.addEventListener('DOMContentLoaded', function(e){
-	document.querySelector('#toggle_menu').addEventListener('click', () => toggleMenu())
-	document.querySelector("#main_menu ul li a[href='/#cursos']").addEventListener('click', function(){
+document.addEventListener('DOMContentLoaded', function (e) {
+	//document.querySelector('#toggle_menu').addEventListener('click', () => toggleMenu())
+	$('nav#main_menu').slideAndSwipe()
+	document.querySelector("#main_menu ul li a[href='/#cursos']").addEventListener('click', function () {
 		this.classList.add('active')
 		toggleMenu()
 		document.querySelector("#main_menu ul li a.active").classList.remove('active')
 	})
 
-	document.querySelector('#FLogin').addEventListener('submit', function(e){
+	document.querySelector('#FLogin').addEventListener('submit', function (e) {
 		e.preventDefault()
 		let datos = serialize(this)
 		axios.post('/API/usuarios/login', datos)
-		.then(res => {
-			if(typeof res.data == 'object'){
-				vue.session = res.data
-				this.reset()
-				$('#MLogin').modal('hide')
-			}else{
-				let login_error = document.querySelector('#login_error')
-				login_error.innerHTML = res.data
-				login_error.classList.add('show')
-				setTimeout(() => {
-					login_error.classList.remove('show')
-				}, 5000)
-			}
-		}).catch(error => {
-			console.log(error)
-		})
+			.then(res => {
+				if (typeof res.data == 'object') {
+					vue.session = res.data
+					this.reset()
+					$('#MLogin').modal('hide')
+				} else {
+					let login_error = document.querySelector('#login_error')
+					login_error.innerHTML = res.data
+					login_error.classList.add('show')
+					setTimeout(() => {
+						login_error.classList.remove('show')
+					}, 5000)
+				}
+			}).catch(error => {
+				console.log(error)
+			})
 	})
 
 	document.querySelector('#newsletter').addEventListener('submit', e => {
@@ -37,14 +38,14 @@ document.addEventListener('DOMContentLoaded', function(e){
 			email
 		}).then(res => {
 			spinner.classList.add('d-none')
-			if(res.data == true){
+			if (res.data == true) {
 				e.target.querySelector('input').value = ''
 				swal.fire(
 					'Listo!',
 					'Recibirá notificaciones por correo sobre informaciones de interes, gracias por utilizar nuestros servicios.',
 					'success'
 				)
-			}else{
+			} else {
 				swal.fire(
 					'Oops!',
 					res.data,
@@ -62,18 +63,65 @@ document.addEventListener('DOMContentLoaded', function(e){
 		})
 	})
 
+	//Aunto hide header on scroll
+	var doc = document.documentElement;
+	var w = window;
+
+	var prevScroll = w.scrollY || doc.scrollTop;
+	var curScroll;
+	var direction = 0;
+	var prevDirection = 0;
+
+	var header = document.querySelector('header#main_header');
+
+	var checkScroll = function () {
+		/*
+		** Find the direction of scroll
+		** 0 - initial, 1 - up, 2 - down
+		*/
+
+		curScroll = w.scrollY || doc.scrollTop;
+		if (curScroll > prevScroll) {
+			//scrolled up
+			direction = 2;
+		}
+		else if (curScroll < prevScroll) {
+			//scrolled down
+			direction = 1;
+		}
+
+		if (direction !== prevDirection) {
+			toggleHeader(direction, curScroll);
+		}
+
+		prevScroll = curScroll;
+	};
+
+	var toggleHeader = function (direction, curScroll) {
+		if (direction === 2 && curScroll > 52) {
+			header.classList.add('hide');
+			prevDirection = direction;
+		}
+		else if (direction === 1) {
+			header.classList.remove('hide');
+			prevDirection = direction;
+		}
+	};
+
+	window.addEventListener('scroll', checkScroll);
+
 	$(".tel").mask("999-999-9999")
-    $(".rnc").mask("9-99-99999-9")
-    $(".dui").mask("999-9999999-9")
+	$(".rnc").mask("9-99-99999-9")
+	$(".dui").mask("999-9999999-9")
 })
 
-function toggleMenu(){
+function toggleMenu() {
 	let activo = document.querySelector('nav#main_menu').classList.contains('active')
 	let button = document.querySelector('#toggle_menu')
-	if(activo){
+	if (activo) {
 		document.querySelector('nav#main_menu').classList.remove('active')
 		button.classList.remove('active')
-	}else{
+	} else {
 		document.querySelector('nav#main_menu').classList.add('active')
 		button.classList.add('active')
 	}
@@ -88,90 +136,90 @@ function format(input, type, decimals = true) {
 			output = `(${tel.substr(0, 3)}) ${tel.substr(3, 3)}-${tel.substr(6, 4)}`
 			break;
 		case 'cash':
-			output =  (decimals) 
+			output = (decimals)
 				? (input).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
 				: input.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 			break;
-		case 'fecha': 
+		case 'fecha':
 			var aFecha = input.split('-');
 			var anio = aFecha[0];
 			var dia = aFecha[2];
 			var mes = aFecha[1];
-			var date = new Date(anio,mes-1,dia);
+			var date = new Date(anio, mes - 1, dia);
 			var diaSemana = date.getUTCDay();
-	
-			switch (diaSemana){
+
+			switch (diaSemana) {
 				case 1:
-					diaSemana='Lunes';
+					diaSemana = 'Lunes';
 					break;
 				case 2:
-					diaSemana='Martes';
+					diaSemana = 'Martes';
 					break;
 				case 3:
-					diaSemana='Miercoles';
+					diaSemana = 'Miercoles';
 					break;
 				case 4:
-					diaSemana='Jueves';
+					diaSemana = 'Jueves';
 					break;
 				case 5:
-					diaSemana='Viernes';
+					diaSemana = 'Viernes';
 					break;
 				case 6:
-					diaSemana='Sabado';
+					diaSemana = 'Sabado';
 					break;
 				case 0:
-					diaSemana='Domingo';
+					diaSemana = 'Domingo';
 					break;
 			}
-	
-			switch (mes){
+
+			switch (mes) {
 				case '01':
-					mes='Enero';
+					mes = 'Enero';
 					break;
 				case '02':
-					mes='Febrero';
+					mes = 'Febrero';
 					break;
 				case '03':
-					mes='Marzo';
+					mes = 'Marzo';
 					break;
 				case '04':
-					mes='Abril';
+					mes = 'Abril';
 					break;
 				case '05':
-					mes='Mayo';
+					mes = 'Mayo';
 					break;
 				case '06':
-					mes='Junio';
+					mes = 'Junio';
 					break;
 				case '07':
-					mes='Julio';
+					mes = 'Julio';
 					break;
 				case '08':
-					mes='Agosto';
+					mes = 'Agosto';
 					break;
 				case '09':
-					mes='Septiembre';
+					mes = 'Septiembre';
 					break;
 				case '10':
-					mes='Octubre';
+					mes = 'Octubre';
 					break;
 				case '11':
-					mes='Noviembre';
+					mes = 'Noviembre';
 					break;
 				case '12':
-					mes='Diciembre';
+					mes = 'Diciembre';
 					break;
 			}
-	
+
 			//output = diaSemana+' '+dia+' de '+mes+' '+anio;
-			output = dia+' de '+mes+' '+anio;
+			output = dia + ' de ' + mes + ' ' + anio;
 			break;
 	}
 
 	return output;
 }
 
-function serialize(form){
+function serialize(form) {
 	let output = {}
 	form.querySelectorAll('input, textarea').forEach(input => {
 		output[input.getAttribute('name')] = input.value
@@ -180,45 +228,45 @@ function serialize(form){
 	return output
 }
 
-function logout(){
+function logout() {
 	axios.get('/API/usuarios/logout')
-	.then(res => {
-		cargarSesion()
-	}).catch(error => {
-		console.error(error)
-	})
+		.then(res => {
+			cargarSesion()
+		}).catch(error => {
+			console.error(error)
+		})
 }
 
-function cargarSesion(){
+function cargarSesion() {
 	axios.get('/API/usuarios/session')
-	.then(res => {
-		vue.session = res.data
-	}).catch(error => {
-		console.log(error)
-	})
+		.then(res => {
+			vue.session = res.data
+		}).catch(error => {
+			console.log(error)
+		})
 }
 
-function validarCedula(cedula){
+function validarCedula(cedula) {
 	let valida = false
-	if(cedula.length == 11){
+	if (cedula.length == 11) {
 		let multiplicado_binario = 1
 		let suma = 0
 
-		for (let i = 0; i < 10; i++){
-            let num = parseInt(cedula.substr(i, 1))
-            let pre_suma = num * multiplicado_binario;
-            if (pre_suma.toString().length == 2){
-                suma += parseInt(pre_suma.toString().substr(0, 1))
-                suma += parseInt(pre_suma.toString().substr(1, 1))
-            }else
-                suma += pre_suma;
+		for (let i = 0; i < 10; i++) {
+			let num = parseInt(cedula.substr(i, 1))
+			let pre_suma = num * multiplicado_binario;
+			if (pre_suma.toString().length == 2) {
+				suma += parseInt(pre_suma.toString().substr(0, 1))
+				suma += parseInt(pre_suma.toString().substr(1, 1))
+			} else
+				suma += pre_suma;
 
-            multiplicado_binario = (multiplicado_binario == 1) ? 2 : 1;
-        }
+			multiplicado_binario = (multiplicado_binario == 1) ? 2 : 1;
+		}
 
-		if (suma.toString().substr(1, 1) == '0' && cedula.substr(10, 1) == '0' || 
-		((parseInt(suma.toString().substr(0, 1)) + 1) * 10 - suma) == parseInt(cedula.substr(10, 1)))
-            valida = true;
+		if (suma.toString().substr(1, 1) == '0' && cedula.substr(10, 1) == '0' ||
+			((parseInt(suma.toString().substr(0, 1)) + 1) * 10 - suma) == parseInt(cedula.substr(10, 1)))
+			valida = true;
 	}
 
 	return valida
